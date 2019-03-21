98db4c84480c79c9b4f7632c164ca781f1d32d62
dandrejvv <dandrejvv@gmail.com>
2019-03-21 22:04:04 +0200

    Managed to leverage the .NET Core Console instead for doing the direct drawing but still having issues with unicode characters

--- ConsoleRenderer/ConsoleRenderer/NativeWindowsScreen.cs
+++ ConsoleRenderer/ConsoleRenderer/NativeWindowsScreen.cs
@@ -1,33 +1,22 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace ConsoleRenderer
 {
     public class NativeWindowsScreen : IScreen
     {
-        [DllImport("Kernel32.dll", SetLastError = true)]
-        private static extern IntPtr CreateConsoleScreenBuffer(
-            UInt32 dwDesiredAccess,
-            UInt32 dwShareMode,
-            IntPtr secutiryAttributes,
-            UInt32 flags,
-            IntPtr screenBufferData);
+        const String KERNEL32 = "kernel32.dll";
 
-        [DllImport("Kernel32.dll", SetLastError = true)]
-        private static extern bool SetConsoleActiveScreenBuffer(IntPtr hConsoleOutput);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
+        [DllImport(KERNEL32, SetLastError = true)]
         static extern bool WriteConsoleOutputCharacter(
           IntPtr hConsoleOutput,
           char[] lpCharacter,
           int nLength,
           COORD dwWriteCoord,
           ref int lpumberOfCharsWritten);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern bool SetConsoleWindowInfo(IntPtr hConsoleOutput,
-            bool absolute, ref SMALL_RECT consoleWindow);
 
         [StructLayout(LayoutKind.Sequential)]
         internal struct COORD
@@ -40,21 +29,6 @@ namespace ConsoleRenderer
             internal short X;
             internal short Y;
         }
-
-        [StructLayoutAttribute(LayoutKind.Sequential)]
-        internal struct SMALL_RECT
-        {
-            internal short Left;
-            internal short Top;
-            internal short Right;
-            internal short Bottom;
-        }
-
-        const uint GENERIC_READ = 0x80000000;
-        const uint GENERIC_WRITE = 0x40000000;
-        const uint FILE_SHARE_READ = 0x00000001;
-        const uint FILE_SHARE_WRITE = 0x00000002;
-        const uint CONSOLE_TEXTMODE_BUFFER = 1;
 
         private char[] _buffer;
         private char[] _emptyBuffer;
@@ -69,9 +43,22 @@ namespace ConsoleRenderer
             _buffer = new char[ScreenWidth * ScreenHeight];
             _emptyBuffer = new char[ScreenWidth * ScreenHeight];
 
-            _consoleHandle = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, IntPtr.Zero, CONSOLE_TEXTMODE_BUFFER, IntPtr.Zero);
 
-            SetConsoleActiveScreenBuffer(_consoleHandle);
+            Console.CursorVisible = false;
+            Console.SetWindowSize(ScreenWidth, ScreenHeight + 1);
+
+            _consoleHandle = GetConsoleHandle();
+        }
+
+        // I'm cheating a little bit. Instead of creating my own handle of the console and trying to setup all the various
+        // bells and whistles, I'm using the normal .NET Core version of the Windows implementation and fetching the handle
+        // from there.
+        private IntPtr GetConsoleHandle()
+        {
+            var consoleType = typeof(Console);
+            var consolePalType = consoleType.Assembly.GetType("System.ConsolePal");
+            var outputHandleProp = consolePalType.GetProperty("OutputHandle", BindingFlags.Static | BindingFlags.NonPublic);
+            return (IntPtr)outputHandleProp.GetValue(null);
         }
 
         public int ScreenWidth { get { return _screenWidth; } private set { _screenWidth = value; } }
@@ -90,7 +77,10 @@ namespace ConsoleRenderer
         public void RenderToScreen()
         {
             int writtenChars = 0;
-            WriteConsoleOutputCharacter(_consoleHandle, _buffer, _buffer.Length, new COORD(0 , 0), ref writtenChars);
+            if(!WriteConsoleOutputCharacter(_consoleHandle, _buffer, _buffer.Length, new COORD(0 , 0), ref writtenChars))
+            {
+                var error = Marshal.GetLastWin32Error();
+            }
             _emptyBuffer.CopyTo(_buffer, 0);
         }
     }
